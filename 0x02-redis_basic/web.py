45d5c870f4ded201bc2429fe042f24fb1ebaaa89
Goldjay94 <genevieveezeh786@gmail.com>
2023-07-19 21:39:24 +0000

    fixed web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,35 +5,37 @@ create a web caching
 import redis
 import requests
 from functools import wraps
-from typing import Callable
+
+store = redis.Redis()
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def url_access_count(method):
+    """decorator for get_page function"""
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url):
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = store.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+            # Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        store.incr(key_count)
+        store.set(key, html_content, ex=10)
+        store.expire(key, 10)
+        return html_content
+    return wrapper
 
 
-@data_cacher
+@url_access_count
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
